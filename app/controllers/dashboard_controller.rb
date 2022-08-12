0332dcba80a6b65aecc6e166d7c263d4f6e5cbc3
arpangoswami4 <arpan.goswami21@gmail.com>
2022-08-12 15:04:25 +0530

    dashboard and helper_methods added,

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class DashboardController < ApplicationController
-    skip_before_action :is_user_logged_in?
-    def index
-    end
-    def about
-    end
+  skip_before_action :is_user_logged_in?
+  
+  def index; end
+
+  def about; end
 end
--- app/controllers/ledgers_controller.rb
+++ app/controllers/ledgers_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class LedgersController < ApplicationController
-  
-
   def index
     @ledgers = Ledger.all if @user
   end
